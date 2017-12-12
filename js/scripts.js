c7488111ff5d9b51a008fdf1485a69439496cf90
mohammed uzair daud <uzairdaud@hotmail.co.uk>
2017-12-12 00:23:32 +0000

    Added few more JS scripts, google map API and new layout for site

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 
     menuButton.addEventListener("click", toggleMenu);
 
-    let toggle = false; // hidden at first
+    let toggle = false;
     function toggleMenu() {
         console.log(toggle);
         if (toggle) { // true: it's visible
@@ -13,7 +13,7 @@
             toggle = false
             console.log(`if true: ${toggle}`);
         }
-        else { // false: it's hidden
+        else { 
             navMenu.classList.add("show-menu"), // show it
             toggle = true
             console.log(`if false: ${toggle}`);
@@ -21,3 +21,46 @@
     }
 
 })();
+"use strict";
+
+function initMap() {
+  let manchester = {lat: 53.4808, lng: -2.2426};
+  let ot = {lat: 53.4630589, lng: -2.2935288};
+  let eti = {lat: 53.4831322, lng: -2.200467};
+  let sal = {lat: 53.5143, lng: -2.2770};
+  let mapDemo = document.getElementById("map");
+  let map = new google.maps.Map(mapDemo, {
+    zoom: 12,
+    center: manchester
+  });
+  let markerOT = new google.maps.Marker({
+    position: ot,
+    map: map,
+    title: 'Manchester United vs Manchester City, Sun 10/12 16:45 Kick Off'
+  });
+  let markerETI = new google.maps.Marker({
+    position: eti,
+    map: map,
+    title: 'Manchester City vs Real Madrid, Wed 13/12 20:00 Kick Off'
+  });
+  let markerSAL = new google.maps.Marker({
+	  position: sal,
+	  map: map,
+	  title: 'Salford City vs Stockport, Sat 06/01 15:00 Kick Off'
+  });
+  
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
