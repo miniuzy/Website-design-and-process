fc76f3703f9a405f50948b99490d9e7f92eac299
mohammed uzair daud <uzairdaud@hotmail.co.uk>
2017-12-29 15:10:24 +0000

    added search engine using guardian api

--- js/scripts.js
+++ js/scripts.js
@@ -61,6 +61,40 @@ function scrollFunction() {
     }
 }
 
+"use strict";
+
+(function(){
+  
+  let queryBox = document.getElementById("guardQuery");
+  let searchForm = document.getElementById("searchForm");
+  let demoJSON = document.getElementById("demo");
+
+  // get an api key from http://open-platform.theguardian.com/access/
+  let baseURL = "https://content.guardianapis.com/search?api-key=cb0d763f-7d5f-4efc-b081-b6b1c953bb25&q=";
+
+  searchForm.addEventListener("submit", function(ev){
+    let url = baseURL + queryBox.value;
+    let request = new Request(url);
+    fetch(request)
+      .then(function (response) {
+        // console.log(`response: ${response.status}`);
+        return response.json();
+      })
+      .then(function(data) {
+        // console.log(data);
+        let key;
+        let theData = "";
+        let tmp = data.response.results;
+        for (key in tmp) {
+          theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
+        }
+        demoJSON.innerHTML = theData;
+      });
+	  queryBox.value = "";
+    ev.preventDefault();
+  }, false);
+
+}());
 
 
 
